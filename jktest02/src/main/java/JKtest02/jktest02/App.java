f17ad922f27a35d502f2c225c49c30519548b580
Jessica <jessicaklinc@hotmail.com>
2018-04-15 10:10:58 +0200

    f

---
+++ jktest02/src/main/java/JKtest02/jktest02/App.java
@@ -0,0 +1,13 @@
+package JKtest02.jktest02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 02!" );
+    }
+}
